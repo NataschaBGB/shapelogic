eb1fd2ebb5d6a415d6a873f765240014a2f8fc8d
NataschaBGB <nannabjerning@hotmail.com>
2026-06-01 11:32:13 +0200

    add: Header component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,43 @@
+import './header.sass';
+import Link from 'next/link';
+
+
+export default function Header() {
+    
+    return (
+        
+        <header className="header">
+
+            <div className="container">
+
+                <Link href="/" className="header__logo">
+                    <img src="shapelogic_logo.png" alt="ShapeLogic Logo" className="header__logo-image" />
+                    <h1 className="header__title">Shape<span>Logic</span></h1>
+                </Link>
+                
+                <nav className="header__nav">
+                    <ul className="header__nav-list">
+                        <li className="header__nav-item">
+                            <Link href="/services" className="header__nav-link">Services</Link>
+                        </li>
+                        <li className="header__nav-item">
+                            <Link href="/about" className="header__nav-link">Om os</Link>
+                        </li>
+                        <li className="header__nav-item">
+                            <Link href="/contact" className="header__nav-link">Kontakt</Link>
+                        </li>
+                        <li className="header__nav-item">
+                            <button className="header__nav-button">Book en konsultation</button>
+                        </li>
+                    </ul>
+                </nav>
+
+            </div>
+
+            
+
+        </header>
+        
+    );
+
+}
